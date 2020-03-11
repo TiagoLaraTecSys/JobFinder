3ba3d56a1aff9e287dbd8a53305c279ba606e3c2
Tiago Ribeiro <laratecsys@gmail.com>
2020-03-11 01:47:57 -0300

    Primeiro commit com domain, repository, servicos, recursos de Categoria

---
+++ src/main/java/com/laratecsys/jobfinder/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.laratecsys.jobfinder.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.laratecsys.jobfinder.domain.Categoria;
+import com.laratecsys.jobfinder.repositories.CategoriaRepositories;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepositories repo;
+
+	public Categoria find(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
