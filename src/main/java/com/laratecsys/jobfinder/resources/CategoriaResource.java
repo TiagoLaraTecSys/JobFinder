1eec6c1404ea845974c8ed443251391868e5e295
Tiago Ribeiro <laratecsys@gmail.com>
2020-03-16 23:19:13 -0300

    Listando todas categorias com CategoriaDTO

--- src/main/java/com/laratecsys/jobfinder/resources/CategoriaResource.java
+++ src/main/java/com/laratecsys/jobfinder/resources/CategoriaResource.java
@@ -1,6 +1,9 @@
 package com.laratecsys.jobfinder.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.laratecsys.jobfinder.domain.Categoria;
+import com.laratecsys.jobfinder.dto.CategoriaDTO;
 import com.laratecsys.jobfinder.services.CategoriaService;
 
 @RestController
@@ -53,5 +57,13 @@ public class CategoriaResource {
 
 			service.delete(id);
 			return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll(){
+
+			List<Categoria> list = service.findAll();
+			List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+			return ResponseEntity.ok().body(listDTO);
 	}
 }
