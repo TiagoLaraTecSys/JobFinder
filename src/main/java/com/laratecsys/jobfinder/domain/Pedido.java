a006fb1c589e572a8d6e7f71190a3638df7c2726
Tiago Ribeiro <laratecsys@gmail.com>
2020-03-16 21:16:39 -0300

    Atualização utilização somente do @JsonIgnore

--- src/main/java/com/laratecsys/jobfinder/domain/Pedido.java
+++ src/main/java/com/laratecsys/jobfinder/domain/Pedido.java
@@ -16,7 +16,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 
@@ -29,13 +28,13 @@ public class Pedido implements Serializable {
 	@JsonFormat(pattern = "dd/MM/yy HH:mm")
 	private Date instantDate;
 	
-	@JsonManagedReference
+	
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
 	
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
-	@JsonManagedReference
+	
 	private Cliente cliente;
 	
 	@ManyToOne
