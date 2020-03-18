d1223702350fd52b623f4ceb4774669ed1a7a8ec
Tiago Ribeiro <laratecsys@gmail.com>
2020-03-18 11:22:35 -0300

    Erro de validação de EMAIL repetido

--- src/main/java/com/laratecsys/jobfinder/repositories/ClienteRepositories.java
+++ src/main/java/com/laratecsys/jobfinder/repositories/ClienteRepositories.java
@@ -3,11 +3,13 @@ package com.laratecsys.jobfinder.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.laratecsys.jobfinder.domain.Cliente;
 
 @Repository
 public interface ClienteRepositories extends JpaRepository<Cliente, Integer>{
 
-	
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/laratecsys/jobfinder/services/validation/ClientInsertValidator.java
+++ src/main/java/com/laratecsys/jobfinder/services/validation/ClientInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.laratecsys.jobfinder.domain.Cliente;
 import com.laratecsys.jobfinder.domain.enums.TipoCliente;
 import com.laratecsys.jobfinder.dto.ClienteNewDTO;
+import com.laratecsys.jobfinder.repositories.ClienteRepositories;
 import com.laratecsys.jobfinder.resources.exception.FieldMessage;
 import com.laratecsys.jobfinder.services.validation.util.BR;
 
 public class ClientInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepositories repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 		
@@ -29,6 +36,11 @@ public class ClientInsertValidator implements ConstraintValidator<ClienteInsert,
 		
 		if (objDTO.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod()) && !BR.isValidTfn(objDTO.getCpfOuCnpj())) {
 			errors.add(new FieldMessage("CpfOuCnpj", "CNPJ inválido", objDTO.getCpfOuCnpj()));
+		}
+		
+		Cliente aux = repo.findByEmail(objDTO.getEmail());
+		if (aux != null) {
+			errors.add(new FieldMessage("Email", "Email ja existe!", objDTO.getEmail()));
 		}
 		
 		for (FieldMessage fieldMessage : errors) {
