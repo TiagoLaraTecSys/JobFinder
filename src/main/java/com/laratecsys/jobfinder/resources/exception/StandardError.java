96d8afd2d5f2147c40f5d7d00e0d3da077df3777
Tiago Ribeiro <laratecsys@gmail.com>
2020-03-18 12:03:43 -0300

    Erro de validação no Email no UPDATE do cliente

--- src/main/java/com/laratecsys/jobfinder/resources/exception/StandardError.java
+++ src/main/java/com/laratecsys/jobfinder/resources/exception/StandardError.java
@@ -2,11 +2,14 @@ package com.laratecsys.jobfinder.resources.exception;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class StandardError implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer status;
 	private String msg;
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
 	private Long timestamp;
 	
 	
--- src/main/java/com/laratecsys/jobfinder/services/validation/ClientUpdateValidator.java
+++ src/main/java/com/laratecsys/jobfinder/services/validation/ClientUpdateValidator.java
@@ -2,44 +2,45 @@ package com.laratecsys.jobfinder.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.laratecsys.jobfinder.domain.Cliente;
-import com.laratecsys.jobfinder.domain.enums.TipoCliente;
-import com.laratecsys.jobfinder.dto.ClienteNewDTO;
+import com.laratecsys.jobfinder.dto.ClienteDTO;
 import com.laratecsys.jobfinder.repositories.ClienteRepositories;
 import com.laratecsys.jobfinder.resources.exception.FieldMessage;
-import com.laratecsys.jobfinder.services.validation.util.BR;
 
-public class ClientInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+public class ClientUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
 
+	@Autowired
+	HttpServletRequest request;
+	
 	@Autowired
 	private ClienteRepositories repo;
 	
 	@Override
-	public void initialize(ClienteInsert ann) {
+	public void initialize(ClienteUpdate ann) {
 		
 	}
 	@Override
-	public boolean isValid(ClienteNewDTO objDTO, ConstraintValidatorContext context) {
+	public boolean isValid(ClienteDTO objDTO, ConstraintValidatorContext context) {
 
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
 
+		Integer mapId = Integer.parseInt(map.get("id"));
+		
 		List<FieldMessage> errors = new ArrayList<>();
 		
-		if (objDTO.getTipo().equals(TipoCliente.PESSOA_FISICA.getCod()) && !BR.isValidSsn(objDTO.getCpfOuCnpj())) {
-			errors.add(new FieldMessage("CpfOuCnpj", "CPF inválido", objDTO.getCpfOuCnpj()));
-		}
-		
-		if (objDTO.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod()) && !BR.isValidTfn(objDTO.getCpfOuCnpj())) {
-			errors.add(new FieldMessage("CpfOuCnpj", "CNPJ inválido", objDTO.getCpfOuCnpj()));
-		}
-		
 		Cliente aux = repo.findByEmail(objDTO.getEmail());
-		if (aux != null) {
+		if (aux != null && !aux.getId().equals(mapId)) {
 			errors.add(new FieldMessage("Email", "Email ja existe!", objDTO.getEmail()));  
 		}
 		
